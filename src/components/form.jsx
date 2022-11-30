658f15b91842442e3c9008330fddc1849148d6c2
lisacee <lisaceedev@gmail.com>
2022-11-29 16:37:24 -0800

    Added form component, SCSS compiling

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,5 +1,34 @@
+import '../styles/style.css';
+import {useState} from "react";
+
 export function CardForm() {
+	const [cardNumber, setCardNumber] = useState( '' )
 	return (
-		<h1>Hi</h1>
+		<form>
+			<label>
+				Cardholder Name
+				<input type={"text"}></input>
+			</label>
+			<label>
+				Card Number
+				<input type={"number"}></input>
+			</label>
+			<fieldset id={"expDate"}>
+				<legend>
+					Exp. Date (mm/yy)
+				</legend>
+				<label>Month
+					<input type={"number"}></input>
+				</label>
+				<label>Year
+					<input type={"number"}></input>
+				</label>
+			</fieldset>
+			<label>
+				CVC
+				<input></input>
+			</label>
+
+		</form>
 	)
 }
